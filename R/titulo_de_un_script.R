bf9e7832a08b2421f90fd000b83495212274c103
joseovandor <39111174+joseovandor@users.noreply.github.com>
2021-08-10 13:23:52 -0500

    imagenes

--- R/titulo_de_un_script.R
+++ R/titulo_de_un_script.R
@@ -1,3 +1,14 @@
 
 
-a = 1
+library(MASS)
+library(ggplot2)
+
+data(cats)
+
+dir.create("mis_imagenes")
+
+pdf("mis_imagenes/cats_plot.pdf")
+ggplot(cats, aes(x = Sex)) +
+    geom_bar(fill = "orange", color = "black") + theme_classic() +
+    xlab("Sexo") + ylab("Número de Gatos") + ggtitle("Gatos")
+dev.off()
